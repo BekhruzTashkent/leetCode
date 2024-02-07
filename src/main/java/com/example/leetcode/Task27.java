e2947fdfb8ad639faeae68d44d2991dcf3bdffaa
BekhruzTashkent <bkarimov414@gmail.com>
2024-02-07 12:40:21 +0500

    first

--- src/main/java/com/example/leetcode/Task27.java
+++ src/main/java/com/example/leetcode/Task27.java
@@ -1,11 +1,25 @@
 package com.example.leetcode;
 
+import java.util.Arrays;
+
 public class Task27 {
     public static void main(String[] args) {
 
+        int[] nums = {3,2,2,3};
+        int val =  3;
 
+        int number = 0;
+        for(int i =0; i<nums.length; i++) {
 
-//hello
-
+            int value = nums[i];
+            if (value == val){
+               continue;
+            }else {
+                number++;
+                nums[number-1] = nums[i];
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        System.out.println(number);
     }
 }
